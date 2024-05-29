4c172654c702a608b189effafddb4a26fd504c01
g0dspee <gu.bergmann@duocuc.cl>
2024-05-29 19:25:12 -0400

    jQuery Validate

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
+$.validator.setDefaults({
+  submitHandler: function () {
+    alert("submitted!");
+  }
+});
+
 // Copia los productos de index.html 20 veces
 
 if (document.getElementById('mini_producto')) {
 
-    var tarjeta = document.getElementById('mini_producto').outerHTML;
+  var tarjeta = document.getElementById('mini_producto').outerHTML;
 
-    var tarjetas = '';
+  var tarjetas = '';
 
-    for (i = 0; i < 20; i++) {
+  for (i = 0; i < 20; i++) {
 
-        tarjetas = tarjetas + tarjeta;
+    tarjetas = tarjetas + tarjeta;
 
-    }
+  }
 
-    document.getElementById('mini_producto').outerHTML = tarjetas;
+  document.getElementById('mini_producto').outerHTML = tarjetas;
 
 }
 
@@ -20,17 +26,17 @@ if (document.getElementById('mini_producto')) {
 
 if (document.getElementById('menu')) {
 
-    fetch('menu_superior.html').then(response => {
+  fetch('menu_superior.html').then(response => {
 
-        return response.text();
+    return response.text();
 
-    }).then(htmlContent => {
+  }).then(htmlContent => {
 
-        document.getElementById('menu').innerHTML = htmlContent;
+    document.getElementById('menu').innerHTML = htmlContent;
 
-        window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 
-    });
+  });
 
 };
 
@@ -38,202 +44,223 @@ if (document.getElementById('menu')) {
 
 if (document.getElementById('footer')) {
 
-    fetch('footer.html').then(response => {
+  fetch('footer.html').then(response => {
 
-        return response.text();
+    return response.text();
 
-    }).then(htmlContent => {
+  }).then(htmlContent => {
 
-        document.getElementById('footer').innerHTML = htmlContent;
+    document.getElementById('footer').innerHTML = htmlContent;
 
-        window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 
-    });
+  });
 
 };
 
 //API
 
-$(document).ready(function() {
-    $.get('http://fakestoreapi.com/products', function(data) {
-        var productList = $('#product-list');
+$(document).ready(function () {
+  $.get('http://fakestoreapi.com/products', function (data) {
+    var productList = $('#product-list');
 
-        var row = $('<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4"></div>');
-        productList.append(row);
+    var row = $('<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4"></div>');
+    productList.append(row);
 
-        data.forEach(function(product, index) {
-            var formattedPrice = formatPrice(product.price);
-            var formattedDescription = formatDescription(product.description);
+    data.forEach(function (product, index) {
+      var formattedPrice = formatPrice(product.price);
+      var formattedDescription = formatDescription(product.description);
 
-            var cardColumn = $('<div class="col"></div>');
-            var card = $('<div class="card"></div>');
-            var image = $('<img class="card-img-top" src="' + product.image + '" alt="' + product.title + '">');
-            var cardBody = $('<div class="card-body"></div>');
-            var title = $('<h5 class="card-title">' + product.title + '</h5>');
-            var description = $('<p class="card-text">' + formattedDescription + '</p>');
-            var price = $('<p class="card-price">' + formattedPrice + '</p>');
-            var button = $('<a href="#" class="btn btn-primary">Ver</a>');
+      var cardColumn = $('<div class="col"></div>');
+      var card = $('<div class="card"></div>');
+      var image = $('<img class="card-img-top" src="' + product.image + '" alt="' + product.title + '">');
+      var cardBody = $('<div class="card-body"></div>');
+      var title = $('<h5 class="card-title">' + product.title + '</h5>');
+      var description = $('<p class="card-text">' + formattedDescription + '</p>');
+      var price = $('<p class="card-price">' + formattedPrice + '</p>');
+      var button = $('<a href="#" class="btn btn-primary">Ver</a>');
 
-            cardBody.append(title);
-            cardBody.append(description);
-            cardBody.append(price);
-            cardBody.append(button);
-            card.append(cardBody);
-            cardColumn.append(image);
-            cardColumn.append(card);
-            row.append(cardColumn);
-        });
+      cardBody.append(title);
+      cardBody.append(description);
+      cardBody.append(price);
+      cardBody.append(button);
+      card.append(cardBody);
+      cardColumn.append(image);
+      cardColumn.append(card);
+      row.append(cardColumn);
     });
+  });
 });
 
 function formatPrice(price) {
-    return '$' + price.toFixed(2);
+  return '$' + price.toFixed(2);
 }
 
 function formatDescription(description) {
-    return description.substring(0, 100) + '...';
+  return description.substring(0, 100) + '...';
 }
 
 fetch('http://fakestoreapi.com/products')
-      .then(response => response.json())
-      .then(data => {
-        const cardsContainer = document.getElementById('cards-container');
-        
-        data.forEach(product => {
-          const card = document.createElement('div');
-          card.className = 'card';
+  .then(response => response.json())
+  .then(data => {
+    const cardsContainer = document.getElementById('cards-container');
 
-          const image = document.createElement('img');
-          image.src = product.image;
-          card.appendChild(image);
+    data.forEach(product => {
+      const card = document.createElement('div');
+      card.className = 'card';
 
-          const title = document.createElement('h3');
-          title.textContent = product.title;
-          card.appendChild(title);
+      const image = document.createElement('img');
+      image.src = product.image;
+      card.appendChild(image);
 
-          const price = document.createElement('p');
-          price.textContent = `$${product.price}`;
-          card.appendChild(price);
+      const title = document.createElement('h3');
+      title.textContent = product.title;
+      card.appendChild(title);
 
-          cardsContainer.appendChild(card);
-        });
-      })
-      .catch(error => console.error('Error:', error));
+      const price = document.createElement('p');
+      price.textContent = `$${product.price}`;
+      card.appendChild(price);
 
-      $(document).ready(function() {
-        $("#formRegistro").submit(function(event) {
-          event.preventDefault();
-          var form = $(this);
-          var isValid = true;
-      
-          // Validación del campo "Nombres"
-          var nombres = $("#nombre").val();
-          if (nombres.trim() === "") {
-            isValid = false;
-            $("#nombre").addClass("is-invalid");
-          } else {
-            $("#nombre").removeClass("is-invalid");
-          }
-      
-          // Validación del campo "Apellidos"
-          var apellidos = $("#apellidos").val();
-          if (apellidos.trim() === "") {
-            isValid = false;
-            $("#apellidos").addClass("is-invalid");
-          } else {
-            $("#apellidos").removeClass("is-invalid");
-          }
-      
-          // Validación del campo "Rut"
-          var rut = $("#rut").val();
-          if (rut.trim() === "") {
-            isValid = false;
-            $("#rut").addClass("is-invalid");
-          } else {
-            $("#rut").removeClass("is-invalid");
-          }
-      
-          // Validación del campo "Email"
-          var email = $("#email").val();
-          var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-          if (email.trim() === "" || !emailPattern.test(email)) {
-            isValid = false;
-            $("#email").addClass("is-invalid");
-          } else {
-            $("#email").removeClass("is-invalid");
-          }
-      
-          // Validación del campo "Contraseña"
-          var password = $("#password").val();
-          var passwordPattern = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-          if (password.trim() === "" || !passwordPattern.test(password)) {
-            isValid = false;
-            $("#password").addClass("is-invalid");
-          } else {
-            $("#password").removeClass("is-invalid");
-          }
-      
-          // Validación del campo "Repetir contraseña"
-          var password2 = $("#password2").val();
-          if (password2.trim() === "" || password2 !== password) {
-            isValid = false;
-            $("#password2").addClass("is-invalid");
-          } else {
-            $("#password2").removeClass("is-invalid");
-          }
-      
-          // Validación del campo "Dirección"
-          var direccion = $("#direccion").val();
-          if (direccion.trim() === "") {
-            isValid = false;
-            $("#direccion").addClass("is-invalid");
-          } else {
-            $("#direccion").removeClass("is-invalid");
-          }
-      
-          // Validación del campo "Imagen de perfil"
-          var imagenPerfil = $("#imagenPerfil").val();
-          if (imagenPerfil.trim() === "") {
-            isValid = false;
-            $("#imagenPerfil").addClass("is-invalid");
-          } else {
-            $("#imagenPerfil").removeClass("is-invalid");
-          }
-      
-          if (isValid) {
-            // El formulario es válido, puedes realizar acciones adicionales aquí
-            alert("Formulario válido");
-          } else {
-            // El formulario no es válido, puedes mostrar mensajes de error o realizar otras acciones
-            alert("Formulario inválido");
-          }
-      
-          form.addClass("was-validated");
-        });
-      });
+      cardsContainer.appendChild(card);
+    });
+  })
+  .catch(error => console.error('Error:', error));
 
 
 
-      
+$(document).ready(function () {
 
-      function validarRut(rut) {
-        rut = rut.replace(/[^\dkK]/g, ''); // Eliminar caracteres no válidos
-        if (rut.length < 2) {
-          return false; // Rut inválido si es muy corto
-        }
-        
-        var cuerpo = rut.slice(0, -1); // Obtener el cuerpo del rut (sin el dígito verificador)
-        var dv = rut.slice(-1).toUpperCase(); // Obtener el dígito verificador (último caracter, convertido a mayúscula)
-        
-        var suma = 0;
-        var multiplicador = 2;
-        for (var i = cuerpo.length - 1; i >= 0; i--) {
-          suma += parseInt(cuerpo.charAt(i)) * multiplicador;
-          multiplicador = multiplicador === 7 ? 2 : multiplicador + 1;
-        }
-        
-        var resultado = 11 - (suma % 11);
-        var digitoVerificadorCalculado = resultado === 11 ? '0' : resultado === 10 ? 'K' : resultado.toString();
-        
-        return dv === digitoVerificadorCalculado;
+
+  // Agregar método de validación para RUT chileno
+  $.validator.addMethod("rutChileno", function (value, element) {
+
+    // Validar que el RUT tenga el formato correcto (8 o 9 dígitos + guión + dígito verificador)
+    var rutPattern = /^\d{7,8}-[\dK]$/;
+    if (!rutPattern.test(value)) {
+      return false;
+    }
+
+    // Validar el dígito verificador
+    var rutSinGuion = value.replace("-", "");
+    var rut = rutSinGuion.slice(0, -1);
+    var dv = rutSinGuion.slice(-1);
+    var factor = 2;
+    var sum = 0;
+    for (var i = rut.length - 1; i >= 0; i--) {
+      sum += parseInt(rut.charAt(i)) * factor;
+      factor = factor === 7 ? 2 : factor + 1;
+    }
+    var dvCalculado = 11 - (sum % 11);
+    dvCalculado = dvCalculado === 11 ? "0" : dvCalculado === 10 ? "K" : dvCalculado.toString();
+
+    return dv === dvCalculado;
+  }, "El RUT no es válido (escriba sin puntos y con guión)");
+
+  // Agregar método de validación para correo
+  $.validator.addMethod("emailCompleto", function (value, element) {
+
+    // Expresión regular para validar correo electrónico
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,}))$/;
+
+    // Validar correo electrónico con la expresión regular
+    return regex.test(value);
+
+  }, 'El formato del correo no es válido');
+
+  // Agregar método de validación para que un campo sólo acepte 
+  // letras y espacios en blanco, pero no números ni símbolos,
+  // ideal para campos como nombres y apellidos
+  $.validator.addMethod("soloLetras", function (value, element) {
+
+    return this.optional(element) || /^[a-zA-Z\s]*$/.test(value);
+
+  }, "Sólo se permiten letras y espacios en blanco.");
+
+
+  // El siguiente Javascript obliga a que la caja de texto del rut, siempre escriba la letra "K" en mayúscula
+  document.getElementById('rut').addEventListener('keyup', function (e) {
+    e.target.value = e.target.value.toUpperCase();
+  });
+
+  $("#formulario-registro").validate({
+    rules: {
+      rut: {
+        required: true,
+        rutChileno: true
+      },
+      nombre: {
+        required: true,
+        soloLetras: true
+      },
+      apellidos: {
+        required: true,
+        soloLetras: true
+      },
+      correo: {
+        required: true,
+        emailCompleto: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+        maxlength: 15,
+      },
+      password2: {
+        required: true,
+        minlength: 5,
+        maxlength: 15,
+        equalTo: "password",
+      },
+      direccion: {
+        required: true
+      },
+    }, // --> Fin de reglas
+    messages: {
+      rut: {
+        required: "El RUT es un campo requerido",
+        rutChileno: "El RUT no es válido (escriba sin puntos y con guión)"
+      },
+      nombre: {
+        required: "El nombre es un campo requerido",
+        soloLetras: "El nombre sólo puede contener letras y espacios en blanco",
+      },
+      apellidos: {
+        required: "Los apellidos son un campo requerido",
+        soloLetras: "Los apellidos sólo pueden contener letras y espacios en blanco",
+      },
+      correo: {
+        required: "El correo es un campo requerido",
+        email: "El formato del correo no es válido",
+      },
+      password: {
+        required: "La contraseña es un campo requerido",
+        minlength: "La contraseña debe tener un mínimo de 5 caracteres",
+        maxlength: "La contraseña debe tener un máximo de 15 caracteres",
+      },
+      password2: {
+        required: "Repetir contraseña es un campo requerido",
+        minlength: "Repetir contraseña debe tener un mínimo de 5 caracteres",
+        maxlength: "Repetir contraseña debe tener un máximo de 15 caracteres",
+        equalTo: "Debe repetir la contraseña escrita anteriormente",
+      },
+      direccion: {
+        required: "Dirección es un campo requerido",
+      },
+
+      submitHandler: function (form) {
+        // Handle form submission here
+        form.submit();
       }
+      
+    },
+
+    errorClass: "is-invalid", // Clase CSS para estilizar los errores
+    errorElement: "div", // Elemento HTML en el que se mostrará el mensaje de error
+    errorPlacement: function(error, element) {
+        error.insertAfter(element);
+    // --> Fin de mensajes
+    }
+  });
+});
+
+
